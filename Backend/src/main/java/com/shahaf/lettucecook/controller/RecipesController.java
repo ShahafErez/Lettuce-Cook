e50fc94c234d2d8cfbc00c9173b35f6e6f517fa2
ShahafErez <Shahaf30@gmail.com>
2023-07-31 17:08:08 +0300

    Revert "wip mapping recipe creation"
    This reverts commit 2a7484680e51b68e43de7a9d0d3e7e44213276d3.

--- Backend/src/main/java/com/shahaf/lettucecook/controller/RecipesController.java
+++ Backend/src/main/java/com/shahaf/lettucecook/controller/RecipesController.java
@@ -2,7 +2,6 @@ package com.shahaf.lettucecook.controller;
 
 import com.shahaf.lettucecook.dto.RecipeCreationDto;
 import com.shahaf.lettucecook.entity.Recipe;
-import com.shahaf.lettucecook.mapper.RecipeMapper;
 import com.shahaf.lettucecook.service.RecipeService;
 import org.apache.catalina.mapper.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,8 +28,6 @@ public class RecipesController {
     @PostMapping("/add")
     public ResponseEntity<String> addRecipe(@RequestBody RecipeCreationDto recipeCreationDto) {
         String name = recipeCreationDto.getName();
-        Recipe recipeCreation = RecipeMapper.MAPPER.recipeDtoToRecipe(recipeCreationDto);
-
 //        Recipe recipe = mapper.toRecipe(recipeCreationDto);
 //        recipeService.addRecipe(recipe);
         return new ResponseEntity<>("ok", HttpStatus.OK);
--- Backend/src/main/java/com/shahaf/lettucecook/entity/Ingredient.java
+++ Backend/src/main/java/com/shahaf/lettucecook/entity/Ingredient.java
@@ -1,11 +1,9 @@
 package com.shahaf.lettucecook.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
 @Table(name = "ingredients")
-@Data
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
