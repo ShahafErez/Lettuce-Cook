9caf7e017b479a2f733294b439a2ea56c7c04b91
ShahafErez <Shahaf30@gmail.com>
2023-11-21 11:52:59 +0200

    Added category change in home page

--- Frontend/lettuce-cook-front/src/pages/Home.js
+++ Frontend/lettuce-cook-front/src/pages/Home.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import RecipesList from "../components/recipe/RecipesList";
 
 export default function Home() {
-  const [category, setCategory] = useState("Dinner");
+  let categoriesList = ["Dinner", "Lunch", "Salad", "Snack", "Dessert"];
+  const [category, setCategory] = useState(categoriesList[0]);
 
   return (
     <React.Fragment>
@@ -14,7 +15,29 @@ export default function Home() {
       <h2 className="recipes-group-title">Newest</h2>
       <RecipesList random="false" />
 
-      <h2 className="recipes-group-title">{category}</h2>
+      <div className="recipes-list-category">
+        <h2 className="recipes-group-title">{category}</h2>
+        {/* Select different category */}
+        <div class="dropdown">
+          <button type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-arrow-left-right"></i>
+          </button>
+          <ul class="dropdown-menu">
+            {categoriesList.map((category, index) => (
+              <li
+                key={index}
+                class="dropdown-item"
+                onClick={() => {
+                  setCategory(category);
+                }}
+              >
+                {category}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
       <RecipesList category={category} random="true" />
     </React.Fragment>
   );
